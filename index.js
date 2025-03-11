8831bd4bddb53e47db6d3ad67e89c78c7cadb68c
Imama Ansari <ansariimama@yahoo.com>
2025-03-11 08:13:13 +0500

    assignment start

---
+++ index.js
@@ -0,0 +1,13 @@
+// PERCENTAGE CALCULATOR 
+let TMarks = +prompt("Write your Total marks here");
+ let obtainedMarks = +prompt("Write your Obtained marks here");
+
+
+// EVEN NUMBER IDENTIFIER
+let num =2
+if (num % 2 == 0) {
+alert("this is even numbers")
+
+} else {
+    alert("this is odd numbers")   
+}
