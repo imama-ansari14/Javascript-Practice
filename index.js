36adb449af57ffaf8b6320a0868c93e24a36b5c7
Imama Ansari <ansariimama@yahoo.com>
2025-03-11 08:40:22 +0500

    user authentication section in process...

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 let TMarks = +prompt("Write your Total marks here");
  let obtainedMarks = +prompt("Write your Obtained marks here");
 
+ let performula = (TMarks / obtainedMarks) * 100;
+if (TMarks="100" || obtainedMarks=85) {
+  console.log("Percentage is 85%");  
+}
 
 // EVEN NUMBER IDENTIFIER
 let num =2
@@ -10,4 +14,14 @@ alert("this is even numbers")
 
 } else {
     alert("this is odd numbers")   
+}
+// User Authentication
+let name1= "ayesha zaheer"
+let pass1=1234
+let name2="warisha amir"
+let pass2=000
+if (name1,name2) {
+    welcome to our old costumer
+} else {
+    thank you
 }
